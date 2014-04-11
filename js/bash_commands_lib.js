fcace686050343a87dc1e49594423199e9cc3f75
asonix <asonix.dev@gmail.com>
2014-04-10 21:35:03 -0500

    got mv working, fixing copy and rm

--- js/bash_commands_lib.js
+++ js/bash_commands_lib.js
@@ -15,7 +15,6 @@ function list(args) {
 
 function makeDirectory(args,type) {
     var path = args[0];
-    console.log(typeof(path));
     for (var i = 0; i < path[0].length; i++) {
         if (path[0][i] == "/") {
             console.log(i);
@@ -62,7 +61,10 @@ function removeDir(args) {
 }
 
 function remove(args) {
-    var dir = handleErrors(search(preparePath(args[0])));
+    var dir = search(preparePath(args[0]),"","folder");
+    if (typeof(dir) == "string") {
+        dir = handleErrors(search(preparePath(args[0]),"","file"));
+    }
     if (dir != false) {
         if (dir.type == "folder") {
             if (dir.contents.length == 0) {
@@ -94,58 +96,40 @@ function move(args) {
     var path2;
     var recursive = false;
     for (var i = 0; i < args.length; i++) {
-        if (args[i] == "-r") {
-            recursive = true;
-        }
-        else if(typeof(path1) != "undefined") {
+        if(typeof(path1) != "undefined") {
             path2 = args[i];
         }
         else {
             path1 = args[i];
+            console.log(path1);
         }
     }
-    if (recursive == true) {
-        var p1 = handleErrors(search(preparePath(path1)));
-        var path2test = [];
-        if (p1 != false) {
-            for (var i = 0; i < preparePath(path2).length-1; i++) {
+    var p1 = search(preparePath(path1),"","file");
+    if (typeof(p1) == "string") {
+        p1 = handleErrors(search(preparePath(path1),"","folder"));
+    }
+    console.log(p1);
+    var path2test = [];
+    if (p1 != false) {
+        for (var i = 0; i < preparePath(path2).length-1; i++) {
+            if (typeof(preparePath(path2)[i]) != "undefined") {
                 path2test.push(preparePath(path2)[i]);
             }
-            var p2test = handleErrors(search(path2test));
-            if (p2test != false) {
-                var p2 = handleErrors(search(preparePath(path2)));
-                if (p2 == false) {
-                    for (var i = 0; i < p1.container.contents.length; i++) {
-                        if (p1.container.contents[i] == p1) {
-                            p1.container.contents.splice(i,1);
-                        }
-                    }
-                    p2test.contents.push(p1);
-                    p1.container = p2;
-                }
-                else {
-                    handleErrors("ERROR: Directory already exists");
-                }
-            }
         }
-    }
-    else {
-        var p1 = handleErrors(search(preparePath(path1)));
-        var path2test;
-        if (p1 != false && p1.contents.length == 0) {
-            for (var i = 0; i < path2.length-1; i++) {
-                path2test.push(path2[i]);
-            }
-            var p2test = handleErrors(search(preparePath(path2test)));
+        console.log(path2test)
+        if (path2test.length != 0) {
+            var p2test = handleErrors(search(path2test,"","folder"));
+            console.log(p2test);
             if (p2test != false) {
-                var p2 = handleErrors(search(preparePath(path2)));
-                if (p2 == false) {
+                var p2 = handleErrors(search(preparePath(path2),"","folder"));
+                console.log(p2);
+                if (p2 != false) {
                     for (var i = 0; i < p1.container.contents.length; i++) {
                         if (p1.container.contents[i] == p1) {
                             p1.container.contents.splice(i,1);
                         }
                     }
-                    p2test.contents.push(p1);
+                    p2.contents.push(p1);
                     p1.container = p2;
                 }
                 else {
@@ -154,7 +138,19 @@ function move(args) {
             }
         }
         else {
-            handleErrors("ERROR: Directory not empty");
+            console.log(path2);
+            var p2 = handleErrors(search(preparePath(path2),"","folder"));
+            console.log(p2);
+            if (p2 != false) {
+                console.log(p1);
+                for (var i = 0; i < p1.container.contents.length; i++) {
+                    if (p1.container.contents[i] == p1) {
+                        p1.container.contents.splice(i,1);
+                    }
+                }
+                p2.contents.push(p1);
+                p1.container = p2;
+            }
         }
     }
 }
